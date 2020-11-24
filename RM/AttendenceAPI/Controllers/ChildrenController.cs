74247e444ffada03dc077c7cab13dd2175d640dc
Basic design creat page <1@DESKTOP-V6KSTL4>
2020-11-24 23:12:59 +0200

    child

---
+++ RM/AttendenceAPI/Controllers/ChildrenController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL;
+using COMMON;
+
+namespace AttendenceAPI.Controllers
+{
+    public class ChildrenController : ApiController
+    {
+        [HttpGet]
+        public List<CChildren> selectChildrenByParentId(string ParentId)
+        {
+            List<CChildren> lc = new List<CChildren>();
+            lc = BChildrenManager.selectChildrenByParentId(ParentId);
+            return lc;
+
+        }
+    }
+}
