35ea20b5549a04d77f621c79bffec47078ee1e62
Basic design creat page <1@DESKTOP-V6KSTL4>
2021-01-30 22:37:17 +0200

    directive

--- Attendence/src/app/phone-mask.directive.ts
+++ Attendence/src/app/phone-mask.directive.ts
@@ -1,4 +1,5 @@
 import { Directive,ElementRef,Renderer2,HostListener } from '@angular/core';
+import { Input } from '@angular/core/src/metadata/directives';
 
 @Directive({
   selector: '[appPhoneMask]'
@@ -6,23 +7,38 @@ import { Directive,ElementRef,Renderer2,HostListener } from '@angular/core';
 export class PhoneMaskDirective {
 
   constructor(private element: ElementRef, private render: Renderer2) { }
+  //@Input('appPhoneMask')
+ 
   @HostListener('keyup') 
-  fun()
+  onkeyup()
   {
-    var str: string;
+  var  str: string;
     str = this.element.nativeElement.value;
     var isNumber = /^[0-9]+$/.test(str);
     if (!isNumber)
     {
       this.render.setProperty(this.element.nativeElement,
-         "value", str.substring(0, str.length - 1));
+         "value", str.substring(0,str.length - 1));
     }
     if (str.length > 9)
     {
-      this.render.setProperty(this.element.nativeElement
-        , "value", str.substring(0, 9));
+     this.render.setProperty(this.element.nativeElement
+       , "value", str.substring(0, 9));
     } 
-    
+ 
   }
+  @HostListener('keydown')
+  keydown()
+  {
+    var  str: string;
+    str = this.element.nativeElement.value;
+    if(str.length<9)
+    {
+      alert("מס' הטלפון אינו חוקי");
+    }
+  }
+
+
+
 
 }
